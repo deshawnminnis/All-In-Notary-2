24ba77680291dc00d333643907d65a128748928d
De'Shawn  X. Minnis <dxm7778@proliancesurgeons.com>
2020-10-30 21:51:08 -0700

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,23 @@
+
+// sidebar slider open
+// const sidebar = document.getElementById("sidebar");
+// const hamburger = document.getElementById("hamburger");
+
+// hamburger.addEventListener("click", () =>{
+//   sidebar.classList.toggle("open");
+
+// });
+
+
+function openSlideMenu() {
+  document.getElementById('sidebar').style.transform = "translateX(0)";
+}
+function closeSlideMenu() {
+  document.getElementById('sidebar').style.transform = "translateX(-400px)";
+}
+
+
+
+
+
+
